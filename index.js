136409bf78f6dc55b4045db5e276c4854147b95e
Luciano Pierdona <lucianompj1@gmail.com>
2020-10-06 17:37:59 -0300

    added comments'

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { consultarCep } = require('correios-brasil');
+const { consultarCep } = require("correios-brasil");
 
+// Default Configuration
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Render Initial Page
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
 });
-app.post("/", (req, res) => {
-    const cepConsultado = req.body.consulta;
 
-    consultarCep(cepConsultado).then((response) => {
-        return res.render("consulta", {cep: response.cep, cidade: response.localidade, uf: response.uf});
+// Result Page
+app.post("/", (req, res) => {
+  const cepConsultado = req.body.consulta;
+
+  // Get data from the correios api
+  consultarCep(cepConsultado).then((response) => {
+    return res.render("consulta", {
+      cep: response.cep,
+      cidade: response.localidade,
+      uf: response.uf,
     });
-})
+  });
+});
 
 app.listen(3000, () => {
-    console.log("server started on port 3000");
-})
+  console.log("server started on port 3000");
+});
--- public/mascara.js
+++ public/mascara.js
@@ -1,3 +1,4 @@
-$(document).ready(function(){
-	$("#cep").mask("99999-999");
+// Mask with jQuery
+$(document).ready(function () {
+  $("#cep").mask("99999-999");
 });
